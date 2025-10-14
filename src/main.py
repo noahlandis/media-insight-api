20725567184227debc91bb9a375cd15c5d6362fd
Noah Landis <noahlandis980@gmail.com>
2025-10-14 18:03:57 -0400

    routing to google callback working

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
 from routers import auth
 
 app = FastAPI()
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=os.environ["OAUTH_SECRET_KEY"],
+    same_site="lax",       
+    https_only=False,
+)
 
 app.include_router(auth.router)
 
--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,12 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from authlib.integrations.starlette_client import OAuth
+
+import os
+oauth = OAuth()
+
+oauth.register(
+    name='google',
+    client_id=os.environ["GOOGLE_CLIENT_ID"],
+    client_secret=os.environ["GOOGLE_CLIENT_SECRET"],
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    client_kwargs={'scope': ["https://www.googleapis.com/auth/youtube.readonly", "https://www.googleapis.com/auth/yt-analytics.readonly"]},
+)
 
 router = APIRouter(
     prefix="/auth"
 )
 
 @router.get("/google")
-async def google():
-    return {"message": "welcome to Google"}
+async def google(request: Request):
+    google = oauth.google
+    redirect_uri = request.url_for("google_callback")
+    return await google.authorize_redirect(request, redirect_uri)
+
+@router.get("/google/callback")
+async def google_callback(request: Request):
+    return {"message": "hello. welcome to google callback"}
 
 @router.get("/reddit")
 async def reddit():
