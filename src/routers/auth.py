0462e7f18db5ebea4323639ca0dd626295dcf843
Noah Landis <noahlandis980@gmail.com>
2025-10-16 20:47:15 -0400

    made generic oauth

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse
 from authlib.integrations.starlette_client import OAuth, OAuthError
 from settings import config
+from enum import Enum
 
 oauth = OAuth()
 
@@ -33,42 +34,9 @@ router = APIRouter(
     prefix="/auth"
 )
 
-@router.get("/google")
-async def google(request: Request):
-    redirect_uri = request.url_for("google_callback")
-    return await oauth.google.authorize_redirect(request, redirect_uri)
-
-@router.get("/google/callback")
-async def google_callback(request: Request):
-    frontend_url = config.frontend_url
-    try:
-        token = await oauth.google.authorize_access_token(request)
-        if 'user' not in request.session:
-            request.session['user'] = {}
-        request.session['user']['google'] = None
-    except OAuthError as e:
-        return RedirectResponse(f"{frontend_url}?error=oauth_failed")
-    return RedirectResponse(frontend_url)
-
-
-@router.get("/reddit")
-async def reddit(request: Request):
-    redirect_uri = request.url_for("reddit_callback")
-    return await oauth.reddit.authorize_redirect(request, redirect_uri, duration="permanent")
-
-
-@router.get("/reddit/callback")
-async def reddit_callback(request: Request):
-    frontend_url = config.frontend_url
-    try:
-        token = await oauth.reddit.authorize_access_token(request)
-        if 'user' not in request.session:
-            request.session['user'] = {}
-        request.session['user']['reddit'] = None
-        print(request.session['user'])
-    except OAuthError as e:
-        return RedirectResponse(f"{frontend_url}?error=oauth_failed")
-    return RedirectResponse(frontend_url) 
+# class Platform(str, Enum):
+#     google = "google"
+#     reddit = "reddit"
 
 @router.get("/me")
 async def get_current_user(request: Request):
@@ -77,3 +45,26 @@ async def get_current_user(request: Request):
         return {"message": "no user found"}
 
     return user
+
+@router.get("/{platform}")
+async def auth(platform: str, request: Request):
+    print(platform)
+    client = oauth.create_client(platform)
+    redirect_uri = request.url_for("auth_callback", platform=platform)
+
+    return await client.authorize_redirect(request, redirect_uri)
+
+@router.get("/{platform}/callback")
+async def auth_callback(platform: str, request: Request):
+    frontend_url = config.frontend_url
+    try:
+        client = oauth.create_client(platform)
+        token = await client.authorize_access_token(request)
+        if 'user' not in request.session:
+            request.session['user'] = {}
+        request.session['user'][platform] = None
+    except OAuthError as e:
+        return RedirectResponse(f"{frontend_url}?error=oauth_failed")
+    return RedirectResponse(frontend_url)
+
+
