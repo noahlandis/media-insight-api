7c5dcae49eaa90342fcfe6d44a1b152b2f5364f2
Noah Landis <noahlandis980@gmail.com>
2025-11-17 18:16:58 -0500

    added data field for metrics to fetch

--- src/models.py
+++ src/models.py
@@ -1,12 +1,29 @@
 from enum import Enum
 from pydantic import BaseModel, Field
-from typing import Literal, Set, Union
+from typing import Literal, Set, Union, Optional
+
+# used to determine the 'part' param to use when calling the youtube/channels API
+CHANNEL_PART = {
+    "view_count": "statistics",
+    "subscriber_count": "statistics",
+    "video_count": "statistics",
+
+    "published_at": "snippet",
+    "name": "snippet",
+    "description": "snippet",
+}
 
 class MediaRequest(BaseModel):
-    visibility_scopes: Set[Literal["public", "private", "unlisted"]] = Field(default_factory=lambda: {"public"}, min_length=1, description="the visibility level to filter the requested resource by")
+    visibility_scopes: Optional[Set[Literal["public", "private", "unlisted"]]] = Field(default_factory=lambda: {"public"}, description="the visibility level to filter the requested resource by", min_length=1)
 
 class ChannelRequest(MediaRequest):
-    pass
+    data: Set[Literal["name", "description", "published_at", "video_count", "view_count", "subscriber_count"]] = Field(description="the data the user wishes to see")
+
+    @property
+    def part(self) -> str:
+        return ",".join({CHANNEL_PART[field] for field in self.data})
+
+
 
 class VideoRequest(MediaRequest):
     pass
