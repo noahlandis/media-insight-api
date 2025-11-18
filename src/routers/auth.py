c141d28b398054fd060aedd4759b72fa84b98dc8
Noah Landis <noahlandis980@gmail.com>
2025-11-18 11:13:31 -0500

    remove '.value' access to enum

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -20,16 +20,16 @@ class Platform(StrEnum):
 
 @router.get("/{platform}")
 async def auth(platform: Platform, request: Request, oauth: OAuthManager = Depends(get_oauth_manager)):
-    client = oauth.create_client(platform.value)
+    client = oauth.create_client(platform)
     if client is None:
         return RedirectResponse(f"{frontend_url}?error=unknown_provider")
-    redirect_uri = request.url_for("auth_callback", platform=platform.value)
+    redirect_uri = request.url_for("auth_callback", platform=platform)
     return await client.authorize_redirect(request, redirect_uri)
 
 @router.get("/{platform}/callback")
 async def auth_callback(platform: Platform, request: Request, settings: Settings = Depends(get_settings), oauth: OAuthManager = Depends(get_oauth_manager), redis = Depends(get_redis)):
     frontend_url = settings.frontend_url
-    client = oauth.create_client(platform.value)
+    client = oauth.create_client(platform)
 
     try:
         provider_response = await client.authorize_access_token(request)
@@ -45,10 +45,10 @@ async def auth_callback(platform: Platform, request: Request, settings: Settings
 
     print(provider_response)
        
-    if platform.value == "reddit":
-        await redis.json().merge(session_key(sid), "$", {platform.value: {"access_token": provider_response.get("access_token")}})
+    if platform == Platform.REDDIT:
+        await redis.json().merge(session_key(sid), "$", {platform: {"access_token": provider_response.get("access_token")}})
     else:
-        await redis.json().merge(session_key(sid), "$", {platform.value: {"access_token": provider_response.get("access_token"), "refresh_token": provider_response.get("refresh_token"), "expires_at": provider_response.get("expires_at")}})
+        await redis.json().merge(session_key(sid), "$", {platform: {"access_token": provider_response.get("access_token"), "refresh_token": provider_response.get("refresh_token"), "expires_at": provider_response.get("expires_at")}})
         # we need to create an inverse mapping since the starlette update_token function isn't session scoped. Without this, we wouldn't know which redis record to update when the token is refreshed
         await redis.set(provider_response.get("refresh_token"), session_key(sid))
     return RedirectResponse(frontend_url)
