52cda64c46a35c91e5c76dcb1b8ba6ae061d043e
Noah Landis <noahlandis980@gmail.com>
2025-11-18 11:39:58 -0500

    using helper to get the alias path

--- src/models.py
+++ src/models.py
@@ -1,16 +1,21 @@
-from enum import Enum
+from enum import StrEnum, auto
 from pydantic import BaseModel, Field, NonNegativeInt, AliasPath, AliasChoices
 from typing import Literal, Set, Union, Optional
+from pydantic.alias_generators import to_camel
+
+class ChannelPartType(StrEnum):
+    SNIPPET = auto()
+    STATISTICS = auto()
 
 # used to determine the 'part' param to use when calling the youtube/channels API
 CHANNEL_PART = {
-    "view_count": "statistics",
-    "subscriber_count": "statistics",
-    "video_count": "statistics",
+    "view_count": ChannelPartType.STATISTICS,
+    "subscriber_count": ChannelPartType.STATISTICS,
+    "video_count": ChannelPartType.STATISTICS,
 
-    "published_at": "snippet",
-    "name": "snippet",
-    "description": "snippet",
+    "published_at": ChannelPartType.SNIPPET,
+    "name": ChannelPartType.SNIPPET,
+    "description": ChannelPartType.SNIPPET,
 }
 
 class MediaRequest(BaseModel):
@@ -23,34 +28,46 @@ class ChannelRequest(MediaRequest):
     def part(self) -> str:
         return ",".join({CHANNEL_PART[field] for field in self.data})
 
+
+
+
+def channel_alias_path(part: ChannelPartType, field_name: str) -> AliasPath:
+    return AliasPath("items", 0, part, to_camel(field_name))
+
 class ChannelResponse(BaseModel):
-    view_count: Optional[NonNegativeInt] = Field(description="the number of views the channel has", default=None, alias=AliasChoices(
-        "viewCount",
-        AliasPath("items", 0, "statistics", "viewCount"),
-    ))
-    subscriber_count: Optional[NonNegativeInt] = Field(description="the number of subscribers the channel has", default=None, alias=AliasChoices(
-        "subscriberCount",
-        AliasPath("items", 0, "statistics", "subscriberCount"),
-    ))
-    video_count: Optional[NonNegativeInt] = Field(description="the number of videos the channel has uploaded", default=None,alias=AliasChoices(
-        "videoCount",
-        AliasPath("items", 0, "statistics", "videoCount"),
-    ))
+    view_count: Optional[NonNegativeInt] = Field(description="the number of views the channel has", default=None, alias=channel_alias_path(ChannelPartType.STATISTICS, "view_count"))
 
-    published_at: Optional[str] = Field(description="the date the channel was created", default=None,alias=AliasChoices(
-        "publishedAt",
-        AliasPath("items", 0, "snippet", "publishedAt"),
-    ))
+    subscriber_count: Optional[NonNegativeInt] = Field(
+        description="the number of subscribers the channel has",
+        default=None,
+        alias=channel_alias_path(ChannelPartType.STATISTICS, "subscriber_count"),
+    )
 
-    name: Optional[str] = Field(description="the name of the channel", default=None,alias=AliasChoices(
-        "title",
-        AliasPath("items", 0, "snippet", "title"),
-    ))
+    video_count: Optional[NonNegativeInt] = Field(
+        description="the number of videos the channel has uploaded",
+        default=None,
+        alias=channel_alias_path(ChannelPartType.STATISTICS, "video_count"),
+    )
 
-    description: Optional[str] = Field(description="the description of the channel", default=None,alias=AliasChoices(
-        "description",
-        AliasPath("items", 0, "snippet", "description"),
-    ))
+    published_at: Optional[str] = Field(
+        description="the date the channel was created",
+        default=None,
+        alias=channel_alias_path(ChannelPartType.SNIPPET, "published_at"),
+    )
+
+    name: Optional[str] = Field(
+        description="the name of the channel",
+        default=None,
+        alias=channel_alias_path(ChannelPartType.SNIPPET, "title"),
+    )
+
+    description: Optional[str] = Field(
+        description="the description of the channel",
+        default=None,
+        alias=channel_alias_path(ChannelPartType.SNIPPET, "description"),
+    )
+
+
 
 
 
