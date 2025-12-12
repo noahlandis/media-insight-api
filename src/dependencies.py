daa7bd228976805ed4a8bd9ff9fb98f8d1cf96f4
Noah Landis <noahlandis980@gmail.com>
2025-12-12 18:15:29 -0500

    getting bearer token if session id not sent

--- src/dependencies.py
+++ src/dependencies.py
@@ -6,6 +6,8 @@ from src.config.oauth_manager import OAuthManager
 from src.config.settings import Settings
 from src.utils import session_key
 from enum import StrEnum, auto
+from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
 
 
 @lru_cache
@@ -19,8 +21,11 @@ def get_oauth_manager(request: Request, redis = Depends(get_redis)):
     oauth_manager = OAuthManager(get_settings(), redis=redis)
     return oauth_manager.oauth
 
-async def get_session_key(request: Request, redis = Depends(get_redis)):
-    session_id = request.session.get("session_id")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_session_key(request: Request, redis = Depends(get_redis), token: Annotated[str, Depends(oauth2_scheme)] = None):
+    session_id = request.session.get("session_id") or token
     if not session_id:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthenticated")
     key = session_key(session_id)
